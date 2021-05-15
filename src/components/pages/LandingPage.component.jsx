f63f674d058bfc6169a9aad1c04a3808647263b5
kap1147 <joshua@theyardapp.com>
2021-05-15 10:22:19 -0400

    updated landing page display

--- src/components/pages/LandingPage.component.jsx
+++ src/components/pages/LandingPage.component.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-
+// Components
 import Navbar from '../navbar/Navbar.container';
-
+// Mui stuff
+import { makeStyles } from '@material-ui/core/styles';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  body: {
+      padding: '0 350px 0 350px',
+      textAlign: 'center',
+  },
+}));
 export default function LandingPage() {
+    const classes = useStyles();
     return (
-        <div className="landing-page">
+        <div className={classes.root}>
             <Navbar />
-            <div className="body"></div>
+            <div className={classes.body}><h3>Hello World!</h3> <p>We are busy at work and will be launching soon!</p><p>Thanks for visiting!</p></div>
             <footer />
         </div>
     )
