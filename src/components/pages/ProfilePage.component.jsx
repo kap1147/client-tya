fc039cb33871c380ee7a7e86911d0d222249204a
kap1147 <joshua@theyardapp.com>
2021-05-15 14:21:59 -0400

    pulled user from state

--- src/components/pages/ProfilePage.component.jsx
+++ src/components/pages/ProfilePage.component.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar.container'
+// Redux
+import { useSelector } from "react-redux";
 
-export default function ProfilePage({user}) {
+export default function ProfilePage() {
+    const {user} = useSelector((state) => state.user);
+
     return (
         <div>
             <Navbar />
