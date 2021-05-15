42d9198da9bcdde789cdaf79750de0b5b56780ff
kap1147 <joshua@theyardapp.com>
2021-05-15 10:56:55 -0400

    testing user prop

--- src/components/pages/ProfilePage.component.jsx
+++ src/components/pages/ProfilePage.component.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Navbar from '../navbar/Navbar.container'
 
-export default function ProfilePage() {
+export default function ProfilePage({user}) {
     return (
         <div>
             <Navbar />
             <h2>Profile Page</h2>
-            
+            {console.log(user)}
         </div>
     )
 }
