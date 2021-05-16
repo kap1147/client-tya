76905187b5cce608c7aa1d079103c4a92c20a491
kap1147 <joshua@theyardapp.com>
2021-05-15 23:19:29 -0400

    updated structure

--- src/components/pages/PrivacyPage.component.jsx
+++ src/components/pages/PrivacyPage.component.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
+import {html} from '../../utils/privacy-policy.html.js'
 // Components
 import Navbar from '../navbar/Navbar.container';
+// Mui stuff
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  body: {
+      padding: '0 350px 0 350px',
+      textAlign: 'center',
+  },
+}));
 
 export default function PrivacyPage() {
+    const classes = useStyles();
+
     return (
-        <div>
+        <div className={classes.root}>
             <Navbar />
-            <h1>Privacy Policy</h1>
+            <div className={classes.body}>
+                {html}
+            </div>
         </div>
     )
 }
