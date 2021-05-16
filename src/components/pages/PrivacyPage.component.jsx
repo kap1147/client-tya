4f82e0b3f0b1a9a93731d7b1a838da3d0f692ba1
kap1147 <joshua@theyardapp.com>
2021-05-15 23:06:00 -0400

    added navbar

--- src/components/pages/PrivacyPage.component.jsx
+++ src/components/pages/PrivacyPage.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+// Components
+import Navbar from '../navbar/Navbar.container';
 
 export default function PrivacyPage() {
     return (
         <div>
+            <Navbar />
             <h1>Privacy Policy</h1>
         </div>
     )
