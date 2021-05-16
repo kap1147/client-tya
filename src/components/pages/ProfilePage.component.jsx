dd43bf2ecca79d07d7246e38966903fe129b2964
kap1147 <joshua@theyardapp.com>
2021-05-15 21:29:19 -0400

    added padding

--- src/components/pages/ProfilePage.component.jsx
+++ src/components/pages/ProfilePage.component.jsx
@@ -2,15 +2,30 @@ import React from 'react'
 import Navbar from '../navbar/Navbar.container'
 // Redux
 import { useSelector } from "react-redux";
+// Mui stuff
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  body: {
+      padding: '0 350px 0 350px',
+      textAlign: 'center',
+  },
+}));
 
 export default function ProfilePage() {
     const {user} = useSelector((state) => state.user);
+    const classes = useStyles();
 
     return (
-        <div>
+        <div className={classes.root}>
             <Navbar />
-            <h2>Profile Page</h2>
-            {console.log(user)}
+            <div className={classes.body}>
+                <h2>Profile Page</h2>
+                <p>Welcome {user.alias}!</p>
+            </div>
         </div>
     )
 }
